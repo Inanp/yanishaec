f33ab7cd68e201412cfd7d4fbabba979385cb8d1
Inanp <inauinalu@gmail.com>
2020-03-31 15:17:13 +0700

    u

--- js/sujs.js
+++ js/sujs.js
@@ -1,16 +1,14 @@
 var click = 0;
 
-if (click = 0){
-  function changeThat() {
-    document.getElementById("weird").innerHTML = "yanishae is not cool";
-  }
-}
-else if (click = 1){
+if (click = 1){
   function changeThat() {
     document.getElementById("weird").innerHTML = "yanishae sucks";
   }
 }
 
+function changeThat(){
+ document.getElementById("weird").innerHTML = "yanishae is not cool";
+}
 function changeButton(){
-  var click = 1;
+  click = 1;
 }
