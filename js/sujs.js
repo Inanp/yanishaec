f35cbc21af819b155969e511de967b387c8f0c93
Inanp <inauinalu@gmail.com>
2020-03-31 15:09:41 +0700

    wow

--- js/sujs.js
+++ js/sujs.js
@@ -1,3 +1,16 @@
-function changeThat(){
-  document.getElementById("weird").innerHTML = "yanishae is not cool";
+var click = 0;
+
+if (click = 0){
+  function changeThat() {
+    document.getElementById("weird").innerHTML = "yanishae is not cool";
+  }
+}
+else if (click = 1){
+  function changeThat() {
+    document.getElementById("weird").innerHTML = "yanishae sucks";
+  }
+}
+
+function changeButton(){
+  click = 1;
 }
